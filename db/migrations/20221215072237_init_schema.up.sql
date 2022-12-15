353e2aa38f1f73a98bd5968389c80544d1fae7c9
dev7BITDNAxyz <dev7@bitdna.xyz>
2022-12-15 19:20:18 +0700

    update db schema

--- db/migrations/20221215072237_init_schema.up.sql
+++ db/migrations/20221215072237_init_schema.up.sql
@@ -2,22 +2,22 @@ CREATE TABLE activities (
     id                  INT             NOT NULL AUTO_INCREMENT,
     email               VARCHAR(255),
     title               VARCHAR(255),
-    created_at          datetime,
-    updated_at          datetime,
-    deleted_at          datetime,
+    created_at          DATETIME,
+    updated_at          DATETIME,
+    deleted_at          DATETIME        NULL DEFAULT NULL,
     PRIMARY KEY (id)
 )ENGINE = InnoDB;
 
 CREATE TABLE todos (
-    id                  INT             NOT NULL AUTO_INCREMENT,
+    id                  INT               NOT NULL AUTO_INCREMENT,
     activity_group_id   INT NOT NULL,
     title               VARCHAR(255),
     is_active           VARCHAR(255),
     priority            VARCHAR(255),
-    created_at          datetime,
-    updated_at          datetime,
-    deleted_at          datetime,
+    created_at          DATETIME,
+    updated_at          DATETIME,
+    deleted_at          DATETIME        NULL DEFAULT NULL,
     PRIMARY KEY (id)
 )ENGINE = InnoDB;
 
-ALTER TABLE todo ADD FOREIGN KEY (activity_group_id) REFERENCES activity(id);
+ALTER TABLE todos ADD FOREIGN KEY (activity_group_id) REFERENCES activities(id);
